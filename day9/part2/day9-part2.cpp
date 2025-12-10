112b8f9264ec3761efe64e1bf5798defc5d9c399
Maggistro <maggistro@googlemail.com>
2025-12-10 15:09:10 +0100

    fixed it!

--- day9/part2/day9-part2.cpp
+++ day9/part2/day9-part2.cpp
@@ -5,29 +5,13 @@
 #include <set>
 #include <map>
 
-
-struct PreviousPosition
-{
-    bool xLeft = false;
-    bool yBottom = false;
-    bool xRight = false;
-    bool yTop = false;
-    std::pair<int, int> lastPoint;
-};
-
 bool isFilled(std::pair<int, int> firstPoint, std::pair<int, int> secondPoint, std::vector<std::pair<int, int>>& cornerPoints)
 {
-    PreviousPosition prevPos;
-    prevPos.lastPoint = cornerPoints.end()[-1];
-
-    // prevPos.xLeft = firstPoint.first < secondPoint.first;
-    // prevPos.xRight = firstPoint.first > secondPoint.first;
-    // prevPos.yBottom = firstPoint.second < secondPoint.second;
-    // prevPos.yTop = firstPoint.second > secondPoint.second;
+    std::pair<int, int> lastPoint = cornerPoints.end()[-1];
 
     for (auto& point : cornerPoints) {
 
-        // completely inside
+        // is inside
         if (point.first > std::min(firstPoint.first, secondPoint.first)
             && point.first < std::max(firstPoint.first, secondPoint.first)
             && point.second > std::min(firstPoint.second, secondPoint.second)
@@ -39,10 +23,10 @@ bool isFilled(std::pair<int, int> firstPoint, std::pair<int, int> secondPoint, s
         if (point.first > std::min(firstPoint.first, secondPoint.first)
             && point.first < std::max(firstPoint.first, secondPoint.first))
         {
-            if ((prevPos.lastPoint.second <= std::min(firstPoint.second, secondPoint.second)
+            if ((lastPoint.second <= std::min(firstPoint.second, secondPoint.second)
             && point.second >= std::max(firstPoint.second, secondPoint.second))
             || (point.second <= std::min(firstPoint.second, secondPoint.second)
-            && prevPos.lastPoint.second >= std::max(firstPoint.second, secondPoint.second))) {
+            && lastPoint.second >= std::max(firstPoint.second, secondPoint.second))) {
                 return false;
             }
         }
@@ -51,73 +35,15 @@ bool isFilled(std::pair<int, int> firstPoint, std::pair<int, int> secondPoint, s
         if (point.second > std::min(firstPoint.second, secondPoint.second)
             && point.second < std::max(firstPoint.second, secondPoint.second))
         {
-            if ((prevPos.lastPoint.first <= std::min(firstPoint.first, secondPoint.first)
+            if ((lastPoint.first <= std::min(firstPoint.first, secondPoint.first)
             && point.first >= std::max(firstPoint.first, secondPoint.first))
             || (point.first <= std::min(firstPoint.first, secondPoint.first)
-            && prevPos.lastPoint.first >= std::max(firstPoint.first, secondPoint.first))) {
+            && lastPoint.first >= std::max(firstPoint.first, secondPoint.first))) {
                 return false;
             }
         }
 
-
-        // previously on rim, move out
-        if (prevPos.xRight || prevPos.xLeft || prevPos.yTop || prevPos.yBottom)
-        {
-            if (prevPos.xRight) {
-                if (point.first < std::max(firstPoint.first, secondPoint.first) && !(prevPos.yTop || prevPos.yBottom)) {
-                    return false;
-                }
-            }
-
-            if (prevPos.xLeft) {
-                if (point.first > std::min(firstPoint.first, secondPoint.first) && !(prevPos.yTop || prevPos.yBottom)) {
-                    return false;
-                }
-            }
-
-            if (prevPos.yTop) {
-                if (point.second < std::max(firstPoint.second, secondPoint.second) && !(prevPos.xLeft || prevPos.xRight)) {
-                    return false;
-                }
-            }
-
-            if (prevPos.yBottom) {
-                if (point.second > std::min(firstPoint.second, secondPoint.second) && !(prevPos.xLeft || prevPos.xRight)) {
-                    return false;
-                }
-            }
-        }
-
-        // on rim, next point only allowed to move out
-        if (
-            (point.first == std::min(firstPoint.first, secondPoint.first) // point has same x as left side
-            || point.first == std::max(firstPoint.first, secondPoint.first))  // or has same x as right side
-            && (point.second >= std::min(firstPoint.second, secondPoint.second) // and is between top and bottom
-            && point.second <=  std::max(firstPoint.second, secondPoint.second))
-
-            || (point.second == std::min(firstPoint.second, secondPoint.second) // point has same y as bottom side
-            || point.second ==  std::max(firstPoint.second, secondPoint.second)) // or has same y as top side
-            && (point.first >= std::min(firstPoint.first, secondPoint.first) // and is is between left and right
-            && point.first <= std::max(firstPoint.first, secondPoint.first))
-        )
-        {
-            prevPos.xLeft = point.first == std::min(firstPoint.first, secondPoint.first);
-            prevPos.yBottom = point.second == std::min(firstPoint.second, secondPoint.second);
-            prevPos.xRight = point.first == std::max(firstPoint.first, secondPoint.first);
-            prevPos.yTop = point.second ==  std::max(firstPoint.second, secondPoint.second);
-            continue;
-        }
-
-        prevPos.xRight = false;
-        prevPos.yTop = false;
-        prevPos.xLeft = false;
-        prevPos.yBottom = false;
-        prevPos.lastPoint = point;
-
-        // outside x range
-        // outside y range
-        // in x range, but next not in y range
-        // in y range, but next not in x range
+        lastPoint = point;
     }
     return true;
 }
@@ -172,4 +98,6 @@ int main() {
 // 4629483216
 // 4600044000
 // 186567246
+// 1704850740
 // 157191288
+// 1525991432
