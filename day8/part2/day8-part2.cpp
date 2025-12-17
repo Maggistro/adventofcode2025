70a656e8e14e39ccfacc2e1cfd756992c2ec6b8b
Maggistro <maggistro@googlemail.com>
2025-12-17 21:39:04 +0100

    Finished day8 part2

--- day8/part2/day8-part2.cpp
+++ day8/part2/day8-part2.cpp
@@ -6,14 +6,17 @@
 #include <cfloat>
 #include <boost/algorithm/string.hpp>
 #include <set>
+#include <map>
 #include <algorithm>
+
+struct Circuit;
 
 struct Point {
     int x;
     int y;
     int z;
 
-    std::set<Point*> connections;
+    Circuit* circuit;
 
     float distanceToPoint(Point* other) {
         return std::sqrt(
@@ -23,130 +26,88 @@ struct Point {
         );
     }
 
-    void addConnection(Point* other) {
-        this->connections.insert(other);
+    std::string toString() {
+        return "(" + std::to_string(x) + "," + std::to_string(y) + "," + std::to_string(z) + ")";
     }
 };
 
-void generateCircuit(Point* point, std::set<Point*>* circuit)
-{
-    for (Point* connection : point->connections) {
-        if (circuit->find(connection) == circuit->end()) {
-            circuit->insert(connection);
-            generateCircuit(connection, circuit);
-        }
-    }
-}
+struct Circuit {
+    std::set<Point*> points;
+};
 
-std::pair<float, std::vector<Point*>> getShortestConnection(std::vector<Point*> points)
+std::pair<Point*, Point*> getShortestConnection(std::set<Circuit*>* circuits)
 {
-    std::pair<float, std::vector<Point*>> connection = std::make_pair(FLT_MAX, std::vector<Point*>{});
-    for (Point* firstPoint : points)
+    std::pair<Point*, Point*> connection = std::make_pair(nullptr, nullptr);
+    std::pair<Circuit*, Circuit*> circuitsToMerge = std::make_pair(nullptr, nullptr);
+
+    float distance = FLT_MAX;
+    for (auto firstCircuit = circuits->begin(); firstCircuit != circuits->end(); ++firstCircuit)
     {
-        float distance = FLT_MAX;
-        Point* closestPoint = nullptr;
-        for (Point* secondPoint : points)
+        std::set<Circuit*>::iterator secondCircuit = std::set<Circuit*>::iterator(firstCircuit);
+        secondCircuit++;
+        for (; secondCircuit != circuits->end(); ++secondCircuit)
         {
-            if (firstPoint != secondPoint
-                && firstPoint->connections.find(secondPoint) == firstPoint->connections.end()
-                && firstPoint->distanceToPoint(secondPoint) < distance)
+            for (Point* firstPoint : (*firstCircuit)->points)
             {
-                distance = firstPoint->distanceToPoint(secondPoint);
-                closestPoint = secondPoint;
-            }
-        }
-
-        if (closestPoint != nullptr)
-        {
-            if (connection.first > distance)
-            {
-                connection = std::make_pair(distance, std::vector<Point*>{firstPoint, closestPoint});
+                for (Point* secondPoint : (*secondCircuit)->points)
+                {
+                    if(firstPoint->distanceToPoint(secondPoint) < distance)
+                    {
+                        circuitsToMerge = std::make_pair(*firstCircuit, *secondCircuit);
+                        connection = std::make_pair(firstPoint, secondPoint);
+                        distance = firstPoint->distanceToPoint(secondPoint);
+                    }
+                }
             }
         }
     }
+    
+    Circuit* newCirctuit = new Circuit();
+    newCirctuit->points.insert(circuitsToMerge.first->points.begin(), circuitsToMerge.first->points.end());
+    newCirctuit->points.insert(circuitsToMerge.second->points.begin(), circuitsToMerge.second->points.end());
+    
+    circuits->erase(circuitsToMerge.first);
+    circuits->erase(circuitsToMerge.second);
+    circuits->insert(newCirctuit);
 
     return connection;
 }
 
-
-
-void buildRim(std::vector<Point*> points)
-{
-    std::vector<std::vector<Point*>> circuits;
-    for (Point* firstPoint: points)
-    {
-        float distance = FLT_MAX;
-        Point* closestPoint = nullptr;
-        for (Point* secondPoint : points)
-        {
-            if (firstPoint != secondPoint
-                && firstPoint->connections.find(secondPoint) == firstPoint->connections.end()
-                && firstPoint->distanceToPoint(secondPoint) < distance)
-            {
-                distance = firstPoint->distanceToPoint(secondPoint);
-                closestPoint = secondPoint;
-            }
-        }
-
-        if (closestPoint != nullptr)
-        {
-            firstPoint->addConnection(closestPoint);
-            closestPoint->addConnection(firstPoint);
-            circuits.push_back(std::vector<Point*>{firstPoint, closestPoint});
-        }
-    }
-
-    
-}
-
 int main() {
     std::fstream in("../data");
-    int subsetSize = 1000;
     std::vector<Point*> points;
+    std::set<Circuit*> circuits;
 
-    for(std::string line; std::getline(in, line);) {
+    for(std::string line; std::getline(in, line);) 
+    {
         std::vector<std::string> tokens;
         boost::split(tokens, line, boost::is_any_of(","));
         points.push_back(new Point{std::stoi(tokens[0]), std::stoi(tokens[1]), std::stoi(tokens[2])});
     }
 
-    std::vector<std::pair<float, std::vector<Point*>>> connections;
-
-    // connect all shortest pairs
-    for (int setSize = 0; setSize < subsetSize; setSize++)
+    for (Point* point : points)
     {
-        std::pair<float, std::vector<Point*>> connection = getShortestConnection(points);
-        connection.second[0]->addConnection(connection.second[1]);
-        connection.second[1]->addConnection(connection.second[0]);
-        connections.push_back(connection);
-    }
-
-    // sort connections by distance
-    std::sort(connections.begin(), connections.end(), [](auto a, auto b) {
-        return a.first < b.first;
-    });
-
-    std::vector<std::pair<float, std::vector<Point*>>> subset = std::vector<std::pair<float, std::vector<Point*>>>(connections.begin(), connections.begin() + subsetSize);
-
-    // create circuits for lowest distance pairs
-    std::set<std::set<Point*>> circuits;
-    std::vector<int> circuitSizes;
-    for (std::pair<float, std::vector<Point*>> connection : subset)
-    {
-        std::set<Point*> circuit;
-        generateCircuit(connection.second[0], &circuit);
+        Circuit* circuit = new Circuit();
+        circuit->points.insert(point);
         circuits.insert(circuit);
     }
 
-    for(std::set<Point*> circuit : circuits)
+    std::cout << "Initial circuits: " << circuits.size() << std::endl;
+    int iteration = 0;
+    int lastCount = circuits.size();
+    while (circuits.size() > 2)
     {
-        circuitSizes.push_back(circuit.size());
+        getShortestConnection(&circuits);
+        if (lastCount != circuits.size())
+        {
+            lastCount = circuits.size();
+            std::cout << "Circuits after iteration " << iteration << ": " << circuits.size() << std::endl;
+        }
     }
 
-    // sort cuircuit sizes
-    std::sort(circuitSizes.begin(), circuitSizes.end());
+    std::pair<Point*, Point*> lastConnection = getShortestConnection(&circuits);
 
-    std::cout << "3 biggest number of circuits multiplied: " << circuitSizes[circuitSizes.size() - 1] * circuitSizes[circuitSizes.size() - 2] * circuitSizes[circuitSizes.size() - 3] << std::endl;
+    std::cout << "Last connection points: " << lastConnection.first->toString() << " - " << lastConnection.second->toString() << std::endl;
 
     return 0;
 }
